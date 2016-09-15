0d32a20a676f60d421c9ab5e218fa76fbd040099
Mark Crossen <markcrossen@studentbody.byu.edu>
2016-09-14 20:20:20 -0600

    cleaned up comments a little

--- hashattack.rb
+++ hashattack.rb
@@ -8,8 +8,13 @@ MAX_STRING_LENGTH = TEST_BIT_SIZES.last/8
 # set up the application path
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
 
-require "sha1wrapper"
 require "benchmark"
+require 'digest/sha1'
+
+def create_digest(string, bit_size)
+  throw "error: bit size must be divisible by 4" if (bit_size % 4 > 0)
+  Digest::SHA1.hexdigest(string)[0..bit_size/4]
+end
 
 print "calculting combinations..."
 ALL_RAND_STRINGS=MAX_STRING_LENGTH.times.map do
@@ -31,9 +36,9 @@ def collision_attack(bit_size)
   found = false
   to_return = Benchmark.measure do
     rstrings1.each do |rstring1|
-      digest1 = HashAttack.create_digest(rstring1, bit_size)
+      digest1 = create_digest(rstring1, bit_size)
       rstrings2.each do |rstring2|
-        if rstring1 != rstring2 && digest1 == HashAttack.create_digest(rstring2, bit_size) then
+        if rstring1 != rstring2 && digest1 == create_digest(rstring2, bit_size) then
           found = true
           break
         end
@@ -48,12 +53,12 @@ end
 def preimage_attack(bit_size)
   # create random preimage and digest to create attack against
   preimage = ALL_RAND_STRINGS.sample
-  digest = HashAttack.create_digest(preimage, bit_size)
+  digest = create_digest(preimage, bit_size)
   rstrings = ALL_RAND_STRINGS.shuffle
   found = false
   to_return = Benchmark.measure do
     rstrings.each do |rstring|
-      if HashAttack.create_digest(rstring, bit_size) == digest && preimage == rstring then
+      if create_digest(rstring, bit_size) == digest && preimage == rstring then
         found = true
         break
       end
@@ -87,6 +92,7 @@ collision_data = Hash[TEST_BIT_SIZES.map do |bit_size|
   [bit_size, to_return]
 end]
 
+puts "writing data to file..."
 File.write(FILE_OUT, "bit size, preimage times, collision times\n" + TEST_BIT_SIZES.map do |bit_size|
   bit_size + ", " + preimage_data[bit_size] + ", " + collision_data[bit_size]
 end.join("\n"))
--- sha1wrapper.rb
+++
@@ -1,7 +0,0 @@
-module HashAttack
-  require 'digest/sha1'
-  def self.create_digest(string, bit_size)
-    throw "error: bit size must be divisible by 4" if (bit_size % 4 > 0)
-    Digest::SHA1.hexdigest(string)[0..bit_size/4]
-  end
-end
