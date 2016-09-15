d9adebc649c07768da215c388941bcb8a224a2e2
Mark Crossen <markcrossen@studentbody.byu.edu>
2016-09-14 18:39:19 -0600

    removed obsolete comment

--- sha1wrapper.rb
+++ sha1wrapper.rb
@@ -1,9 +1,6 @@
 module HashAttack
   require 'digest/sha1'
   def self.create_digest(string, bit_size)
-    # string = string.bytes.map do |n|
-    #   n.to_s(16)
-    # end.join("")
     throw "error: bit size must be divisible by 4" if (bit_size % 4 > 0)
     Digest::SHA1.hexdigest(string)[0..bit_size/4]
   end
